af24a63b33254a2e8b5174fee1f7a107acbf0ac2
Nicola Barban <nicolabarban@users.noreply.github.com>
2025-06-20 18:13:44 +0200

    Update main.sh

--- tinyllama/main.sh
+++ tinyllama/main.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #SBATCH --job-name=job
 #SBATCH --mail-type=all
-#SBATCH --mail-user=$USER@unibo.it
+#SBATCH --mail-user=n.barban@unibo.it
 #SBATCH --nodes=1
-#SBATCH --partition=a100
+#SBATCH --partition=l40s
 #SBATCH --cpus-per-task=64
 #SBATCH --gpus=1
 #SBATCH --mem=256G
-#SBATCH --output=/scratch.hpc/YOUR.NAME/dgit/tinyllama/logs/job.log
-#SBATCH --error=/scratch.hpc/YOUR.NAME/dgit/tinyllama/logs/job.err
+#SBATCH --output=/scratch.hpc/nicola.barban2/HPC_bolo/tinyllama/logs/job.log
+#SBATCH --error=/scratch.hpc/nicola.barban2/HPC_bolo/tinyllama/logs/job.err
 
 mkdir -p /scratch.hpc/$USER/HPC_bolo/tinyllama/cache
 export APPTAINER_TMPDIR=/scratch.hpc/$USER/HPC_bolo/tinyllama/cache
 export APPTAINER_CACHEDIR=/scratch.hpc/$USER/HPC_bolo/tinyllama/cache
 export OMP_NUM_THREADS=64
-cd /scratch.hpc/$USER/HPC_bolo/tinyllama
+cd /scratch.hpc/nicola.barban2/HPC_bolo/tinyllama
 
 apptainer exec --nv container/container.sif python test_tasks.py
 
